39c89479ae48e20c64fceda6ea21156fe0cb476b
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-05-19 23:22:56 -0500

    Added js for user word input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const wordText = document.querySelector(".word"),
   hintText = document.querySelector(".hint span"),
-  refreshBtn = document.querySelector(".refresh-word");
+  inputField = document.querySelector("input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
+
+let correctWord;
 
 const initGame = () => {
   let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
@@ -12,8 +16,25 @@ const initGame = () => {
   }
   wordText.innerText = wordArray.join(""); // passing shuffled word as word text
   hintText.innerText = randomObj.hint; // passing random object hint as hint text
+  correctWord = randomObj.word.toLowerCase(); // passing random word to correctWord
+  inputField.value = ""; // making input field empty
+  inputField.setAttribute("maxlength", correctWord.length); // setting input maxlength attr value to word length
   console.log(randomObj);
 };
 initGame();
 
+const checkWord = () => {
+  let userWord = inputField.value.toLocaleLowerCase();
+  if (!userWord) return alert("Please enter a word"); // if user did not enter anything
+
+  // if user word does not match with the correct word
+  if (userWord !== correctWord)
+    return alert(`Oops! ${userWord} is not a correct word`);
+
+  // if above two if conditions failed then show congrats alert because user word is correct
+  alert(`Congrats! ${userWord.toUpperCase()} is a correct word`);
+};
+initGame();
+
 refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
